d630b0fb823f29543590a88cce9bfd21ace8951a
Werner Vermeylen <werner.vermeylen@exsertus.be>
2012-09-18 20:32:25 +0200

    Pushing new changes

--- source/app/web/CommandRegistry.cs
+++ source/app/web/CommandRegistry.cs
@@ -1,10 +1,24 @@
-﻿namespace app.web
+﻿using System.Collections.Generic;
+
+namespace app.web
 {
   public class CommandRegistry : IFindCommands
   {
+      private IEnumerable<IProcessOneRequest> commands;
+      public CommandRegistry(IEnumerable<IProcessOneRequest> commands)
+      {
+          this.commands = commands;
+      }
+
     public IProcessOneRequest get_the_command_that_can_process(IEncapsulateRequestDetails request)
     {
-      throw new System.NotImplementedException();
+        foreach (var processOneRequest in commands)
+        {
+            if (processOneRequest.can_run(request)) 
+                return processOneRequest;
+        }
+
+        return null;
     }
   }
 }
