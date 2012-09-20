8d335a8ca8dbce323d89f8d13be3eaae53e08541
Werner Vermeylen <werner.vermeylen@exsertus.be>
2012-09-20 16:30:13 +0200

    Pushing new changes

--- source/app/utility/container/DependencyFactories.cs
+++ source/app/utility/container/DependencyFactories.cs
@@ -7,15 +7,25 @@ namespace app.utility.container
   public class DependencyFactories : IFindFactoriesForDependencies
   {
     IEnumerable<ICreateOneDependency> possible_factories;
+    MissingFactory_Behaviour missing_factory_behaviour;
 
-    public DependencyFactories(IEnumerable<ICreateOneDependency> possible_factories)
+    public DependencyFactories(IEnumerable<ICreateOneDependency> possible_factories, MissingFactory_Behaviour missing_factory_behaviour)
     {
       this.possible_factories = possible_factories;
+      this.missing_factory_behaviour = missing_factory_behaviour;
     }
 
     public ICreateOneDependency get_factory_that_can_create(Type dependency)
     {
-      return possible_factories.First(x => x.can_create(dependency));
+        try
+        {
+            return possible_factories.First(x => x.can_create(dependency));
+        }
+        catch
+        {
+            throw missing_factory_behaviour(dependency);
+        }
     }
+
   }
 }
